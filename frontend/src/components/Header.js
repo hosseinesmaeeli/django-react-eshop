20e7def436960b311826553f1a6919d1ed0bb825
hosseinesmaeeli <h.esmaeeli@yahoo.com>
2022-01-10 21:42:32 +0330

    make Header & footer Component

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+//rfce
+function Header() {
+    return (
+        <div>
+            <header>This is a header</header>
+        </div>
+    )
+}
+
+export default Header
+
