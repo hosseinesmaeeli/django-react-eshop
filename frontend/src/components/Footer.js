2fad0c8937a1cf24298d0bdebd9afc00d67959cf
Hossein <92952483+hosseinesmaeeli@users.noreply.github.com>
2022-01-12 08:23:09 +0330

    use fontawsome in heaser & footer
    https://react-bootstrap.github.io/components/navbar/
    https://cdnjs.com/libraries/font-awesome
    https://fontawesome.com/v5.15/icons?d=gallery&p=2&q=shopping&m=free

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 function Footer() {
-    return (
-        <div>
-            <footer>This is footer</footer>
-        </div>
-    )
+  return (
+    <footer>
+      <Container>
+        <Row>
+          <Col className="text-center py-3">
+            Copyright &copy; Hossein Esmaeeli
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,29 @@
-import React from 'react'
+import React from "react";
+import { Navbar, Nav, Container, Row } from "react-bootstrap";
 //rfce react functions components export
 function Header() {
-    return (
-        <div>
-            <header>This is a header</header>
-        </div>
-    )
+  return (
+    <div>
+      <header>
+        <Navbar bg="dark" variant="dark" expand="lg" colapseOnselect>
+          <Container>
+            <Navbar.Brand href="#home">E-Shop</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="/cart">
+                  <i className="fas fa-shopping-cart"> </i>Cart
+                </Nav.Link>
+                <Nav.Link href="/cart">
+                  <i className="fas fa-user"> </i>Login
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </header>
+    </div>
+  );
 }
 
-export default Header
-
+export default Header;
