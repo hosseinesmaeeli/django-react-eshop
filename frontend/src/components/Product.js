53f4d794e578ca5ef9b34206c86f542142c25746
hosseinesmaeeli <h.esmaeeli@yahoo.com>
2022-01-14 08:41:48 +0330

    rating

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Card } from "react-bootstrap";
+import Rating from "./Rating";
 
 function Product({ product }) {
   return (
     <div>
       <Card className="p-3 my-3 rounded">
         <a href={`/product/${product._id}`}>
-          <Card.Img src={product.image} />
+          <Card.Img src={product.image} className="card-img-top" />
         </a>
 
         <Card.Body>
@@ -16,9 +17,13 @@ function Product({ product }) {
             </Card.Title>
           </a>
           <Card.Text as="div" ClassName="my-3">
-            {product.rating} from {product.numReviews} reviews
+            <Rating
+              value={product.rating}
+              text={`${product.numReviews} reviews`}
+              color={"#F8E825"}
+            />
           </Card.Text>
-          <Card.Text as="h4">${product.price}</Card.Text>
+          <Card.Text as="h3">${product.price}</Card.Text>
         </Card.Body>
       </Card>
     </div>
