98c4274fa6ba6315672caa228d300433020caede
Hossein <92952483+hosseinesmaeeli@users.noreply.github.com>
2022-01-18 12:32:13 +0330

    get id from patch in product screen page
    we change in toturial

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import products from "../products";
+import { useParams } from 'react-router-dom';
 
 function ProductScreen() {
-    return (
-        <div>
-            PRODUCT SCREEN
-        </div>
-    )
+  // const product = products.find((p) => p._id === match.params.id);
+  const { id } = useParams();
+  const product = products.find((p) => p._id === (id));
+  return <div>{product.name}</div>;
 }
 
-export default ProductScreen
+export default ProductScreen;
