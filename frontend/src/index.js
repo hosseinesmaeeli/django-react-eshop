7929ce09343015879a47f4bcefcf42fb388944c2
Hossein <92952483+hosseinesmaeeli@users.noreply.github.com>
2022-01-26 08:14:51 +0330

    create store in redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import store from "./store";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import "./bootstrap.min.css";
@@ -8,7 +10,9 @@ import reportWebVitals from "./reportWebVitals";
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
 
