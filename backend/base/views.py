0f361d67a8f4d176d3a0a497e8b875c020d7304a
hosseinesmaeeli <h.esmaeeli@yahoo.com>
2022-02-12 15:49:45 +0330

    JWT-authentication
    generate username & email to use in frontend

--- backend/base/views.py
+++ backend/base/views.py
@@ -10,17 +10,22 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
-    @classmethod
-    def get_token(cls, user):
-        token = super().get_token(user)
+    # @classmethod
+    # def get_token(cls, user):
+    #     token = super().get_token(user)
 
-        # Add custom claims
-        token['username'] = user.username
-        token['message'] = 'hello world'
-        # ...
+    #     # Add custom claims
+    #     token['username'] = user.username
+    #     token['message'] = 'hello world'
+    #     # ...
 
-        return token
+    #     return token
+    def validate(self,attrs):
+        data = super().validate(attrs)
+        data['username'] = self.user.username
+        data['email'] = self.user.email
 
+        return data
 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
