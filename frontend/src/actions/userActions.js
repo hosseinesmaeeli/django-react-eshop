0f3ba72a0cc66fb108a89635370773f915577f09
hosseinesmaeeli <h.esmaeeli@yahoo.com>
2022-03-02 13:31:44 +0330

    user login screen and message

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,17 +1,17 @@
-import axios from "axios";
 import {
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
   USER_LOGIN_FAIL,
   USER_LOGOUT,
 } from "../constants/userConstants";
+import axios from "axios";
 
 export const login = (email, password) => async (dispatch) => {
   try {
     dispatch({ type: USER_LOGIN_REQUEST });
     const config = {
       headers: {
-        "Content-type": "application/json",
+        "Content-type": "application/json"
       },
     };
 
--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link ,useNavigate} from "react-router-dom";
 import { Form, Button, Row, Col } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import FormContainer from "../components/FormContainer";
@@ -7,34 +7,37 @@ import Loader from "../components/Loader";
 import Message from "../components/Message";
 import { login } from "../actions/userActions";
 
-function LoginScreen() {
+function LoginScreen({location}) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const dispatch = useDispatch();
 
-  const redirect = location.search ? location.search.split("=")[1] : "/";
+  const redirect = 'location.search ? location.search.split("=")[1] : "/"';
   const userLogin = useSelector((state) => state.userLogin);
-  const { error, loading, userInfo } = userLogin;
+  const { loading,error,userInfo } = userLogin;
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (userInfo) {
-      history.push(redirect);
+      navigate(redirect);
     }
-  }, [history, userInfo, redirect]);
+  }, [navigate, userInfo, redirect]);
 
 
   const submitHandler = (e) => {
     e.preventDefault();
-    console.log('SuBBBBBBBBBBBBBBBB')
+    // console.log('SuBBBBBBBBBBBBBBBB')
  
-    // dispatch(login(email, password));
+    dispatch(login(email, password));
   };
   return (
     <FormContainer>
       <h1>Sign In</h1>
-      {error && <Message variant="danger">{error}</Message>}
-      {loading && <Loader />}
+     
+      {error && <Message variant="danger" text={error} />}
+      {loading && <Loader/>}
+      
       <Form onSubmit={submitHandler}>
         <Form.Group controlId="email">
           <Form.Label>Email Address</Form.Label>
