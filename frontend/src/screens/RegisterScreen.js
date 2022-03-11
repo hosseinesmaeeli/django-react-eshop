a124a27e1c05ff94f06edcf96fcfcfcb481584b2
hosseinesmaeeli <h.esmaeeli@yahoo.com>
2022-03-11 11:34:46 +0330

    5. Update Profile Endpoint
    we make request : /api/v1/users/profile/update/ in postman and use to test chane profile

--- frontend/src/screens/RegisterScreen.js
+++ frontend/src/screens/RegisterScreen.js
@@ -40,7 +40,7 @@ export default function RegisterScreen({location}) {
 
   return (
     <FormContainer>
-      <h1>Sign In</h1>
+      <h1>Sign Up</h1>
       {message && <Message variant="danger" text={message} />}
       {error && <Message variant="danger" text={error} />}
       {loading && <Loader />}
