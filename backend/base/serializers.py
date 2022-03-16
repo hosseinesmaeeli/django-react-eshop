1646dd3f47cf387a34d0a1b4b10ea38fe99f54cc
hosseinesmaeeli <h.esmaeeli@yahoo.com>
2022-03-16 18:13:51 +0330

    5. Order View & URL (django)

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Product
+from .models import Product, Order,OrderItem,ShippingAddress
 from django.contrib.auth.models import User
 from rest_framework_simplejwt.tokens import RefreshToken
 
@@ -39,4 +39,44 @@ class UserSerializerWithToken(UserSerializer):
 class ProductSerializer(serializers.ModelSerializer):
     class Meta :
         model = Product
-        fields= '__all__'        
+        fields= '__all__'     
+
+class ShippingAddressSerializer(serializers.ModelSerializer):
+    class Meta :
+        model = ShippingAddress
+        fields= '__all__'   
+        
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta :
+        model = OrderItem
+        fields= '__all__'  
+
+class OrderSerializer(serializers.ModelSerializer):
+    orders = serializers.SerializerMethodField(read_only=True)
+    shippingAddress = serializers.SerializerMethodField(read_only=True)
+    user = serializers.SerializerMethodField(read_only=True)
+
+    class Meta :
+        model = Order
+        fields= '__all__'  
+
+def get_orders(self,obj) :
+    items = obj.orderitem_set.all()
+    serializer = OrderItemSerializer(items,many=True)
+    return serializer.data
+
+
+def get_orders(self,obj) :
+    try: 
+        address = ShippingAddressSerializer(obj.shippingAddress, many=False)
+    except:
+        address = False
+
+    return address
+
+def get_user(self,obj) :
+    user = obj.user
+    serializer = UserSerializer(user,many=False)
+    return serializer.data    
+
+        
