eeb2b38b72575783efce0135f9ee31c5a065bc10
hosseinesmaeeli <h.esmaeeli@yahoo.com>
2022-03-18 12:51:36 +0330

    4. Update To Paid Endpoint

--- backend/base/views/order_views.py
+++ backend/base/views/order_views.py
@@ -7,6 +7,7 @@ from base.models import Product, Order, OrderItem, ShippingAddress
 from base.serializers import ProductSerializer, OrderSerializer
 
 from rest_framework import status
+from datetime import datetime
 
 
 @api_view(['POST'])
@@ -71,3 +72,15 @@ def getOrderById(request, pk):
                      status=status.HTTP_400_BAD_REQUEST)
     except:
         return Response({'detail': 'Order does not exist'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def addOrderItems(request,pk):
+    order = Order.objects.get(_id=pk)
+
+    order.isPaid = True
+    order.paidAt = datetime.now()     
+    order.save()   
+    
+    return Response('Order was paid')
